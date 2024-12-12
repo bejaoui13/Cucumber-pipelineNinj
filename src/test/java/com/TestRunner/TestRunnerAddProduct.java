396c3493f62a5b06537505d8349386557e76e832
unknown <ssafa197@gmail.com>
2024-12-12 18:54:44 +0100

    pages pb

--- src/test/java/com/TestRunner/TestRunnerAddProduct.java
+++ src/test/java/com/TestRunner/TestRunnerAddProduct.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		   features = "src/test/resources/features", 
+		   features = "src/test/resources/features/01_Registration.feature", 
           glue = "com.stepDefinition", 
  
           plugin = {"pretty",
